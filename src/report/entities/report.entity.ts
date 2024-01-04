15ce3013f5d12a1859b72639b6bd7dc62de48447
thampaponn <88426179+thampaponn@users.noreply.github.com>
2024-01-04 13:27:34 +0700

    Update report.entity.ts

--- src/report/entities/report.entity.ts
+++ src/report/entities/report.entity.ts
@@ -1,1 +1,34 @@
-export class Report {}
+import { Column, Entity, ObjectIdColumn } from "typeorm";
+
+export enum ReportStatus {
+    PENDING = "pending",
+    APPROVED = "done",
+  }
+
+@Entity({ name: 'report' })
+export class Report {
+    @ObjectIdColumn()
+    _id: string;
+
+    @Column()
+    userId: string;
+
+    @Column()
+    roomId: string;
+
+    @Column()
+    createAt: Date;
+
+    @Column()
+    updatedAt: Date;
+    
+    @Column()
+    detail: string;
+
+    @Column({
+        type: "enum",
+        enum: ReportStatus,
+        default: ReportStatus.PENDING
+    })
+    status: ReportStatus
+}
